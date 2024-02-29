aba9b8f5ea62831d87a660ab755f640107bfc06b
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-02-29 10:26:49 +0200

    fix

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
 # puts ARGV[0].scan(/hbt{2,5}n/).join("\n")
 # puts ARGV[0].scan(/\b(\w*(\w)\2\w*)\b/).join("\n")
-puts ARGV[0].scan(/\b(\w*(\w)\2\w*)\b/).map { |match| match[0] }.join("")
+puts ARGV[0].scan(/\b(\w*(\w)\2\w*)\b/).map { |match| match[0] }.join
 # puts ARGV[0].scan(/\b(\w*(\w)\2{1,4}\w*)\b/).map { |match| match[0] }.join("\n")
--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\b(hb{1,3}tn)\b/).join("")
+puts ARGV[0].scan(/\b(hb{1,3}tn)\b/).join
 
