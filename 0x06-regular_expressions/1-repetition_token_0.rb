108b9a4dc083036fc9a4f8b0400762f17cc34ef8
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-02-29 10:22:36 +0200

    tasks

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join("\n")
+# puts ARGV[0].scan(/hbt{2,5}n/).join("\n")
 # puts ARGV[0].scan(/\b(\w*(\w)\2\w*)\b/).join("\n")
-# puts ARGV[0].scan(/\b(\w*(\w)\2\w*)\b/).map { |match| match[0] }.join("\n")
+puts ARGV[0].scan(/\b(\w*(\w)\2\w*)\b/).map { |match| match[0] }.join("\n")
 # puts ARGV[0].scan(/\b(\w*(\w)\2{1,4}\w*)\b/).map { |match| match[0] }.join("\n")
 
 Find the regular expression that will match the following cases 
